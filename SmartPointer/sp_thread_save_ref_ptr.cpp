4c54f3758bd2dbc74ffd8e674403adf054f3638a
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-03-02 14:34:43 +0900

    [Add] new file

---
+++ SmartPointer/sp_thread_save_ref_ptr.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file sp_thread_save_ref_ptr.cpp
+ * @author Shinichi Inoue (inoue.shinichi.1800@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <thread_save_ref_ptr.hpp>
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main(int, char**)
+{
+    try
+    {
+        /* code */
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return 0;
+}
